ff8dfc2ab10466f6b4d7c5abc1eeef47bfabc54d
VictorTubei <vktubei@gmail.com>
2025-01-16 18:33:33 +0300

    Completed array find method

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(record) {
+    const win = record.find(game => game.result === "W");
+    return win ? win.year : undefined;
+}
+
+const record = [
+    { year: "2023", result: "L" },
+    { year: "2024", result: "W" },
+    { year: "2025", result: "L" }
+];
+
+console.log(superbowlWin(record));
